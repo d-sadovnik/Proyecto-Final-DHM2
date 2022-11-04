6968f8e3ed5135a05aaeead43ceab6795730413c
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-11-04 01:55:10 +0000

    tabla user y profile listas

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, Table, Float
+from sqlalchemy.orm import declarative_base, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user_table"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    profile = db.relationship("Profile", back_populates="name", uselist=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +21,32 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name,
         }
+
+class Profile(db.Model):
+    __tablename__ = "profile_table"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_table.id"))
+    age = db.Column(db.Integer, unique=True, nullable=False)
+    height = db.Column(db.Float, unique=True, nullable=False)
+    weight = db.Column(db.Float, unique=True, nullable=False)
+    bmi = db.Column(db.Float, unique=True, nullable=False)
+    fat_percentage = db.Column(db.Float, unique=True, nullable=False)
+    name = db.relationship("User", back_populates="profile")
+
+    def __repr__(self):
+        return f'<Profile {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "age": self.age,
+            "height": self.height,
+            "weight": self.weight,
+            "bmi": slef.bmi,
+            "fat_percentage": self.fat_percentage,
+        }
+
--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import sqlalchemy 
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -11,6 +12,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 #from models import Person
 
