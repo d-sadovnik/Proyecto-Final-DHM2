acf9338dc634f15265a824944f86a61bd331d334
d-sadovnik <borixe@gmail.com>
2022-11-12 02:49:17 +0000

    routines view phase 1

---
+++ src/front/js/pages/routines.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import defaultroutine from "../../img/defaultcard.png";
+import freeroutine from "../../img/freecard.png";
+import "../../styles/routines.css";
+
+export const Routines = () => {
+    const [type, setType] = useState(2);
+
+  return (
+    <div className={type === 0 ? "containerRoutines0" : type === 1 ? "containerRoutines1" : "containerRoutines"}>
+      <div className="content">
+        
+        <div className="imgDefault">
+          <img src={defaultroutine} 
+          alt="..." 
+          className="defaultpic" 
+          onMouseEnter={() => {
+                setType(0);
+              }}
+            onMouseLeave={() => {
+                setType(2);
+              }}
+          />
+        </div>
+        <div className="imgFree">
+          <img src={freeroutine} 
+          alt="..." 
+          className="freepic"
+          onMouseEnter={() => {
+            setType(1);
+          }} 
+          onMouseLeave={() => {
+            setType(2);
+          }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
