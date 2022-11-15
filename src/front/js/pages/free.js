5d93a5e03a750ce616e82fbd0599437bb223b2f4
d-sadovnik <borixe@gmail.com>
2022-11-15 00:16:35 +0000

    free routine view almost set, missing bakground

--- src/front/js/pages/free.js
+++ src/front/js/pages/free.js
@@ -1,10 +1,68 @@
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+import background from "./../../img/picfondo2.jpg";
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/free.css";
 
 export const Free = () => {
-    return(
-        <div>
+  return (
+    <div>
+      <>
+        <section
+          className="containerfree"
+          style={{ backgroundImage: `url(${background})` }}
+        >
+          <div className="form login">
+            <div className="form-content">
+              <header>Select Muscles</header>
 
-        </div>
-    )}
+              <form action="#">
+              <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Shoulders</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Triceps</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Biceps</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Chest</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Back</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Legs</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+                <div className="muscles">
+                  <input type="checkbox" id="muscleCheck" name="interest" value="biceps" />
+                  <label for="biceps">Core</label>
+                  <input type="number" id="numberOfexercises" name="numberofexercises" placeholder="#" />
+                </div>
+
+                <div className="field button-field">
+                  <button>Generate Routine</button>
+                </div>
+                
+              </form>
+            </div>
+          </div>
+        </section>
+      </>
+    </div>
+  );
+};
