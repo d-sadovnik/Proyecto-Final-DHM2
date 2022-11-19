702e597a284f6e737212f13c38c607ef1d331ccf
d-sadovnik <borixe@gmail.com>
2022-11-19 05:35:37 +0000

    se llama dayroutine la view donde salen las cards con los ejercicios

--- src/front/js/pages/dayroutine.js
+++ src/front/js/pages/dayroutine.js
@@ -1,36 +1,40 @@
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
-import background from "./../../img/abstract-orange-and-black.jpg";
-import React, { useContext, useState } from "react";
+import background from "./../../img/dark-background-for-home.jpg";
+import cardbackground from "./../../img/dayroutine_background.jpg"
+import React, { useContext, useState } from "react"; 
 import { Link } from "react-router-dom";
-import "../../styles/free.css";
+import "../../styles/dayroutine.css";
 
 export const Dayroutine = () => {
   return (
     <div>
       <>
         <section
-          className="containerfree"
+          className="containerdayroutine overflow-auto flex-nowrap"
           style={{ backgroundImage: `url(${background})` }}
         >
-          <div className="contentFree">
-              <header className="freeHeader">Target Muscles</header>
-
-              <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
+          {/* // this is where the cards are at// */}
+          <div className="card col-2 m-2 cardStyle bg-image card shadow-1-strong" style={{ backgroundImage: `url(${cardbackground})` }}>
+            <img
+              src={
+                "https://fitnessprogramer.com/wp-content/uploads/2021/06/Incline-Dumbbell-Row.gif"
+              }
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Exercise - Muscle</h5>
+              <div className="container d-flex ">
+                <div className="float-start"></div>
+                <div className="float-end">
+                  <p>This is the description of the above mentioned exercise</p>
                 </div>
-                
-                <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
-                </div>
-
-                <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
-                </div>
+              </div>
             </div>
+          </div>
+          {/* // this is where the cards end// */}
+        
         </section>
       </>
     </div>
