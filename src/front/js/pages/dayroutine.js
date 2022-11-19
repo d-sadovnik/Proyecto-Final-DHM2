281e2c810d7e7ed5968695c1d420891db35535a2
d-sadovnik <borixe@gmail.com>
2022-11-19 01:30:40 +0000

    dayroutine page created

--- src/front/js/pages/dayroutine.js
+++ src/front/js/pages/dayroutine.js
@@ -3,32 +3,32 @@ import { useNavigate } from "react-router-dom";
 import background from "./../../img/abstract-orange-and-black.jpg";
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import "../../styles/free.css";
+import "../../styles/dayroutine.css";
 
 export const Dayroutine = () => {
   return (
     <div>
       <>
         <section
-          className="containerfree"
+          className="containerdayroutine"
           style={{ backgroundImage: `url(${background})` }}
         >
-          <div className="contentFree">
-              <header className="freeHeader">Target Muscles</header>
+          <div className="contentDayroutine">
+              <header className="dayroutineHeader">Target Muscles</header>
 
               <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
+                  <label className="dayroutineLabel" for="biceps">Exercise</label>
+                  <label className="dayroutineLabel" for="biceps">Muscles</label>
                 </div>
                 
                 <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
+                  <label className="dayroutineLabel" for="biceps">Exercise</label>
+                  <label className="dayroutineLabel" for="biceps">Muscles</label>
                 </div>
 
                 <div className="muscles">
-                  <label className="freeLabel" for="biceps">Exercise</label>
-                  <label className="freeLabel" for="biceps">Muscles</label>
+                  <label className="dayroutineLabel" for="biceps">Exercise</label>
+                  <label className="dayroutineLabel" for="biceps">Muscles</label>
                 </div>
             </div>
         </section>
